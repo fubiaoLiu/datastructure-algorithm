d3a6f2271316f2e14117aac53304ca007987ea59
liufubiao <342654315@qq.com>
2019-12-30 18:06:59 +0800

    feat: 栈、树相关练习提交

---
+++ src/main/java/tree/TreeNode.java
@@ -0,0 +1,16 @@
+package tree;
+
+/**
+ * @description: 树节点
+ * @author: FuBiaoLiu
+ * @date: 2019/12/30
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
