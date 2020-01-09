159524aca69737cb6f42fb60ff6130d58e2ac2ba
liufubiao <342654315@qq.com>
2020-01-09 18:09:21 +0800

    feat: 二叉搜索树相关练习提交

---
+++ src/main/test/tree/_110_Test.java
@@ -0,0 +1,26 @@
+package tree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class _110_Test {
+    _110_平衡二叉树 bst = new _110_平衡二叉树();
+
+    @Test
+    public void should_return_true_given_tree0() {
+        assertThat(bst.isBalanced1(BinarySearchTreeNodeUtil.tree0())).isTrue();
+    }
+    @Test
+    public void should_return_true_given_tree1() {
+        assertThat(bst.isBalanced1(BinarySearchTreeNodeUtil.tree1())).isTrue();
+    }
+    @Test
+    public void should_return_true_given_tree12() {
+        assertThat(bst.isBalanced1(BinarySearchTreeNodeUtil.tree12())).isTrue();
+    }
+    @Test
+    public void should_return_false_given_tree5() {
+        assertThat(bst.isBalanced1(TreeNodeUtil.tree5())).isFalse();
+    }
+}
